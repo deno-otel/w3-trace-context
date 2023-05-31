f17cd0d89be9cf854e549951b9a38a8adb1a465a
Dan Rumney <dancrumb@gmail.com>
2023-05-31 08:52:40 -0500

    docs: ✏️ brief description of TraceParentData

--- parse-trace-parent.ts
+++ parse-trace-parent.ts
@@ -2,6 +2,9 @@ import { InvalidError } from "./exceptions/invalid-error.ts";
 import { UnparseableError } from "./exceptions/unparseable-error.ts";
 import { uint8ArrayFromHexString } from "./typed-array-from-hexstring.ts";
 
+/**
+ * This represents the constituent parts of a traceparent header
+ */
 export interface TraceParentData {
   version: number;
   traceId: Uint8Array;
@@ -30,17 +33,17 @@ export const parseTraceParent = (asString: string): TraceParentData => {
   const [, traceId, parentId, traceFlags, ...extraFields] = asString.split("-");
   if (traceId?.length !== 32) {
     throw new UnparseableError(
-      "Trace ID found in traceparent is the wrong length",
+      "Trace ID found in traceparent is the wrong length"
     );
   }
   if (parentId?.length !== 16) {
     throw new UnparseableError(
-      "Parent ID found in traceparent is the wrong length",
+      "Parent ID found in traceparent is the wrong length"
     );
   }
   if (traceFlags?.length !== 2) {
     throw new UnparseableError(
-      "TraceFlags found in traceparent is the wrong length",
+      "TraceFlags found in traceparent is the wrong length"
     );
   }
 

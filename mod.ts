863ea3589e7c0e44927990738548c8b48816daff
Dan Rumney <dancrumb@gmail.com>
2023-05-31 08:51:42 -0500

    feat: 🎸 export TraceParentData

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export { W3TraceContext } from "./w3-trace-context.ts";
 export { type TraceContext } from "./trace-context.ts";
 export { TraceFlags } from "./trace-flags.ts";
+export { type TraceParentData } from "./parse-trace-parent.ts";
